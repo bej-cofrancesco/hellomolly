c3133d220466997fb80f339e3cdfcc333a46e08f
Benjamin Wilson <benjamin.wilson@acidgreen.com.au>
2024-02-09 18:00:55 +1100

    created header component

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,30 +1,27 @@
-import { HeaderProps } from "@/types/types";
+import { HeaderComponentProps } from "@/types/types";
 import Button from "../common/atoms/Button";
 import { IconCart } from "@/components/common/atoms/Icons"
 
-export default function Header({ logo, navigation, login }: HeaderProps) {
+export default function Header({ logo, navigation, login }: HeaderComponentProps) {
   return (
-    <header role="banner" className="container flex justify-between items-center mt-[54px]">
-      {logo ? <div className="font-elephnt text-h6 text-primary">{logo}</div> : null}
-      <nav className="hidden md:flex">
-        <ul className="flex gap-[58px]">
-          {navigation ? navigation.map((link: string, index) => (
-            <li key={`${link}-${index}`} className="uppercase font-popmed text-body2">
+    <header role="banner" className="flex justify-between items-center">
+      <div className="font-elephnt text-h6 text-primary">{logo}</div>
+      <nav className="hidden lg:flex">
+        <ul className="flex gap-10 xl:gap-[58px]">
+          {navigation.map((link: string, index) => (
+            <li key={`${link}-${index}`} className="uppercase font-popmed text-body2 text-primary">
               <a href="/">{link}</a>
             </li>
-          )) : null}
+          ))}
         </ul>
       </nav>
       <div className="flex gap-[57px]">
         <button aria-label="cart" className="font-size">
           <IconCart />
         </button>
-        {login 
-          ? <Button variant="inverse" size="sm">
-              {login}
-            </Button> 
-          : null
-        }
+        <Button variant="inverse" size="sm">
+          {login}
+        </Button> 
       </div>
     </header>
   )
