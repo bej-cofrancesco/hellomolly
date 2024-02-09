2c0289f70b7a2e03a7626f252b6a49c21e7bf6ef
Benjamin Wilson <benjamin.wilson@acidgreen.com.au>
2024-02-09 18:05:07 +1100

    set up a section for inside the hero

---
+++ src/components/common/molecules/CtaSection.tsx
@@ -0,0 +1,20 @@
+import { CtaSectionProps } from "@/types/types";
+import Heading from "../atoms/Heading";
+import Button from "../atoms/Button";
+
+
+export default function CtaSection({ heading, description, cta }: CtaSectionProps) {
+  return (
+    <div className="flex flex-col max-w-[649px] shrink-0">
+      <Heading as="h1" className="font-rufinabold text-primary text-h2 pb-[27px]">
+        {heading}
+      </Heading>
+      <p className="font-popmed text-secondaryLight text-body3 pb-[54px] max-w-[565px]">
+        {description}
+      </p>
+      <Button size="lg" variant="primary" shadow="primary">
+        {cta}
+      </Button>
+    </div>
+  )
+}
